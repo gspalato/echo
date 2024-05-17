27977d7696a7c05832caa97045fcbb85a444cba5
Gabriel <3950465+gspalato@users.noreply.github.com>
2024-05-17 20:40:54 -0300

    Updated transaction model BSON keys.

--- internal/structures/transaction.go
+++ internal/structures/transaction.go
@@ -8,10 +8,10 @@ const (
 )
 
 type Transaction struct {
-	TransactionType TransactionType `json:"transaction_type"`
-	UserId          string          `json:"user_id"`
-	ClaimId         string          `json:"claim_id"`
-	Credits         float32         `json:"credits"`
-	Timestamp       int64           `json:"timestamp"`
-	Description     string          `json:"description"`
+	TransactionType TransactionType `json:"transaction_type" bson:"transaction_type"`
+	UserId          string          `json:"user_id"          bson:"user_id"`
+	ClaimId         string          `json:"claim_id"         bson:"claim_id"`
+	Credits         float32         `json:"credits"          bson:"credits"`
+	Timestamp       int64           `json:"timestamp"        bson:"timestamp"`
+	Description     string          `json:"description"      bson:"description"`
 }
